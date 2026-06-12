c739a7d38beece608a69186f19f473e102b6f0b0
xquevedo3p <xavi_quevedo@hotmail.com>
2026-06-12 09:19:26 +0200

    feat: complete install React frontend

---
+++ frontend/3PPASSWORD.FRONTEND/src/types/credential.ts
@@ -0,0 +1,9 @@
+export interface Credential {
+    id: string;
+    systemName: string;
+    username: string;
+    url: string;
+    notes: string;
+    createdAt: string;
+    updateAt: string;
+}
